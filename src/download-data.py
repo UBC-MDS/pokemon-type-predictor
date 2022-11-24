1ed8b416ec45917a5453edbe49b1c111809268e0
missarah <sarah_s96@hotmail.com>
2022-11-24 10:12:55 -0800

    fixed issue from pull request
    deleted index_col=1 and rewrote files

--- src/download-data.py
+++ src/download-data.py
@@ -13,12 +13,12 @@ from docopt import docopt
 opt = docopt(__doc__)
 
 def main(url, out_file):
-    data = pd.read_csv(url, index_col=1)
+    data = pd.read_csv(url)
     try:
         data.to_csv(out_file, index=False)
     except:
-        os.makedirs(os.path.dirname(out_file))
-        data.to_csv(out_file, index=False)
+        os.makedirs(os.path.dirname(out_file, index=False))
+        data.to_csv(out_file)
 
 
 if __name__ == "__main__":
